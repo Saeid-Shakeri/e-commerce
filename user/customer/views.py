5a1fd29663303c042dcc0886eff5df1a5586cbbf
Saeid-Shakeri <saeidshakeribcm1@gmail.com>
2024-10-24 01:06:35 +0330

    added ApiViews

---
+++ user/customer/views.py
@@ -0,0 +1,62 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.generics import CreateAPIView, RetrieveUpdateAPIView
+from rest_framework.permissions import IsAuthenticated
+from .models import Customer
+from .serializers import *
+
+
+class CustomerRegisterView(CreateAPIView):
+    queryset = Customer.objects.all()
+    serializer_class = CustomerRegisterSerializer
+
+
+class ProfileView(RetrieveUpdateAPIView):
+    queryset = Customer.objects.all()
+    serializer_class = ProfileSerializer
+    permission_classes = (IsAuthenticated,)
+    http_method_names = ['get', 'patch']
+
+    def get_object(self):
+        return self.request.user
+
+    def patch(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', True)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        return Response({
+            "message": "Object partially updated successfully",
+        }, status=status.HTTP_200_OK)
+
+
+class ChangePasswordView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request, *args, **kwargs):
+        serializer = ChangePasswordSerializer(data=request.data)
+        if serializer.is_valid():
+            user = request.user
+            if not user.check_password(serializer.data.get("old_password")):
+                return Response({"old_password": "Wrong password."}, status=status.HTTP_400_BAD_REQUEST)
+            user.set_password(serializer.data.get("new_password"))
+            user.save()
+            return Response({"status": "password set"}, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ChangeEmailView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request, *args, **kwargs):
+        serializer = ChangeEmailSerializer(data=request.data)
+        if serializer.is_valid():
+            user = request.user
+            user.email = serializer.data.get("email")
+            user.save()
+            return Response({"status": "email set"}, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
