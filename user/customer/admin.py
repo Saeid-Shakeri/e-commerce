977b73a8bb39f6d869703ed5f5ccf6ac0da18e92
Saeid-Shakeri <saeidshakeribcm1@gmail.com>
2024-10-25 23:58:01 +0330

    added customer profile to admin panel

--- user/customer/admin.py
+++ user/customer/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Customer
 
-# Register your models here.
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    exclude =('password','groups',)
+    # search_fields = ['username']
