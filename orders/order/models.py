7dfbc3189f3b57c1d49ab52ae73237343954888e
Saeid-Shakeri <saeidshakeribcm1@gmail.com>
2024-10-28 16:14:00 +0330

    added models

---
+++ orders/order/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+class Order(models.Model):
+    user_id = models.PositiveIntegerField()
+    total_price = models.PositiveIntegerField()
+    status = models.CharField(max_length=10, choices=[
+        ("incomplete", "Incomplete"), ('paid', 'Paid'), ('canceled', 'Cancelled')]
+        , default='incomplete')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order {self.id} - User {self.user_id}"
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    product_id = models.PositiveIntegerField()
+    quantity = models.PositiveIntegerField()
+    price = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"OrderItem {self.id} - Order {self.order.id} - Product {self.product_id}"
