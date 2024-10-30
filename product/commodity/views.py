ec0fa4fb593f24d47133d9fc310a1f70ab358abc
Saeid-Shakeri <saeidshakeribcm1@gmail.com>
2024-10-31 00:37:11 +0330

    added ProductSearch ApiView

--- product/commodity/views.py
+++ product/commodity/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework import filters
 from .models import Category, Product
 from .serializers import CategorySerializer, ProductSerializer
 
@@ -7,9 +8,18 @@ class CategoryListView(generics.ListAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
+
+class ProductSearchView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name']
+
+
 class ProductListView(generics.ListAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
 
 class ProductDetailView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
