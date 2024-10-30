bbcecade1359c621e08715dd687c21deab473f37
Saeid-Shakeri <saeidshakeribcm1@gmail.com>
2024-10-31 00:37:36 +0330

    added product-search url

--- product/commodity/urls.py
+++ product/commodity/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import CategoryListView, ProductListView, ProductDetailView
+from .views import CategoryListView, ProductListView, ProductDetailView, ProductSearchView
 
 
 urlpatterns = [
     path('categories/', CategoryListView.as_view(), name='category-list'),
     path('products/', ProductListView.as_view(), name='product-list'),
+    path('product_search/', ProductSearchView.as_view(), name='product-search'),
     path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
 ]
