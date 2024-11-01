3ea21e23b49b8374e24e077909cebf3647fed0f4
Saeid-Shakeri <saeidshakeribcm1@gmail.com>
2024-11-02 02:21:58 +0330

    added urls for admin

--- product/commodity/urls.py
+++ product/commodity/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import (CategoryListView, ProductListView,
-        ProductDetailView, ProductSearchView, ProductDetailAdminView, ProductListCreateView)
+        ProductDetailView, ProductSearchView, ProductDetailAdminView, ProductListCreateView,
+        CategoryListCreateView)
 
 
 urlpatterns = [
@@ -8,7 +9,8 @@ urlpatterns = [
     path('products/', ProductListView.as_view(), name='product-list'),
     path('product_search/', ProductSearchView.as_view(), name='product-search'),
     path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
-    path('product_create/', ProductListCreateView.as_view(), name='product-create'),
-    path('product_detail_admin/', ProductDetailAdminView.as_view(), name='product-detail-admin'),
+    path('category-admin', CategoryListCreateView.as_view(), name='category-admin'),
+    path('product-list-admin/', ProductListCreateView.as_view(), name='product-list-admin'),
+    path('product-detail-admin/', ProductDetailAdminView.as_view(), name='product-detail-admin'),
 
 ]
