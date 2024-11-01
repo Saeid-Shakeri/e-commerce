f7874f41a345fd76ab995fe110973588a5dbeec6
Saeid-Shakeri <saeidshakeribcm1@gmail.com>
2024-11-02 01:39:41 +0330

    connect all services to one db

--- product/commodity/models.py
+++ product/commodity/models.py
@@ -1,12 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
+
+class Customer(AbstractUser):
+    email = models.EmailField(max_length=254)
+    phone = models.CharField(max_length=14,null=True,blank=True)
+    
+    class Meta:
+        db_table = "customers"
 
 class Category(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
+    
+    class Meta:
+        db_table = "categories"
 
-    def __str__(self):
-        return self.name
+    
+
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
@@ -16,5 +27,27 @@ class Product(models.Model):
     in_stock = models.BooleanField(default=True)
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.name
+    class Meta:
+        db_table = "products"
+
+
+
+class Order(models.Model):
+    user_id = models.ForeignKey(Customer,on_delete=models.CASCADE)
+    total_price = models.PositiveIntegerField(default=0)
+    status = models.CharField(max_length=10, choices=[
+        ("incomplete", "Incomplete"), ('paid', 'Paid'), ('failed', 'Failed'), ('canceled', 'Cancelled')]
+        , default='incomplete')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = "orders"
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    price = models.PositiveIntegerField()
+
+    class Meta:
+        db_table = "order_items"
