ff74bb98fd85279ed1c6a15691ce606be8cc8570
Saeid-Shakeri <saeidshakeribcm1@gmail.com>
2024-11-02 02:39:53 +0330

    updated views and  add OrderCrate view

--- orders/order/views.py
+++ orders/order/views.py
@@ -1,23 +1,34 @@
 from rest_framework import generics
-from .models import Order, OrderItem
-from .serializers import OrderSerializer, OrderItemSerializer
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from .models import Order, OrderItem, Product
+from .permissions import IsAdmin
+from .serializers import (OrderSerializer, OrderItemSerializer, AddToCartSerializer, 
+    OrderCrateSerializer)
 
 
 class OrderListCreateView(generics.ListCreateAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    permission_classes = (IsAdmin,)
 
 
 class OrderDetailView(generics.RetrieveAPIView):
-    queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        user = self.request.user
+        return Order.objects.filter(user_id=user.id)
 
 
-class OrderItemListCreateView(generics.ListCreateAPIView):
-    queryset = OrderItem.objects.all()
-    serializer_class = OrderItemSerializer
+class OrderCreateView(APIView):
+    serializer_class = OrderCrateSerializer
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAuthenticated,)
 
-
-class OrderItemDetailView(generics.RetrieveAPIView):
-    queryset = OrderItem.objects.all()
-    serializer_class = OrderItemSerializer
+    def post(self, request):
+        order = Order.objects.create(user_id=request.user)
+        return Response({'result':'new order created succesfully', 'new_order_id':{order.id}})
