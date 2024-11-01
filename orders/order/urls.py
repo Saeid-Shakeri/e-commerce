15e39b47855663787b32e347455f07ce102ddf99
Saeid-Shakeri <saeidshakeribcm1@gmail.com>
2024-11-02 02:36:38 +0330

    update all urls

--- orders/order/urls.py
+++ orders/order/urls.py
@@ -1,9 +1,22 @@
 from django.urls import path
-from .views import OrderListCreateView, OrderDetailView, OrderItemListCreateView, OrderItemDetailView
+from .views import (OrderListCreateView, OrderDetailView, AddToCartView,
+     UserOrderListView, OrderCreateView)
 
 urlpatterns = [
-    path('orders/', OrderListCreateView.as_view(), name='order-list-create'),
+
+    # list and create for admin
+    path('orders-admin/', OrderListCreateView.as_view(), name='order-list-create'),
+
+    # add an item to cart
+    path('orders/add-to-cart/', AddToCartView.as_view(), name='add-to-cart'),
+
+    # user orders list
+    path('user-orders/', UserOrderListView.as_view(), name='user-order-list'),
+
+    # user an order detail
     path('orders/<int:pk>/', OrderDetailView.as_view(), name='order-detail'),
-    path('order-items/', OrderItemListCreateView.as_view(), name='order-item-list-create'),
-    path('order-items/<int:pk>/', OrderItemDetailView.as_view(), name='order-item-detail'),
+
+    # create new order 
+    path('new-order/', OrderCreateView.as_view(), name='create-new-order'),
+
 ]
