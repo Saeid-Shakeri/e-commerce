62827b292e12d55d45804f09f6dfe94e5ddf4757
Saeid-Shakeri <saeidshakeribcm1@gmail.com>
2024-11-12 01:13:29 +0330

    added some tests

--- notification/send_mail/tests.py
+++ notification/send_mail/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+# class MyViewTest(TestCase): 
+#     def test_my_view_status_code(self):
+#         response = self.client.get(reverse('send-email'))
+#         self.assertEqual(response.status_code, 200)
