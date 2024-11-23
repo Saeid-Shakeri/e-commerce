43ce897b2ebf8089eba8abdb8f5e228c417464c9
Saeid-Shakeri <saeidshakeribcm1@gmail.com>
2024-11-23 14:13:23 +0330

    added urls

--- user/customer/urls.py
+++ user/customer/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
-from .views import (CustomerRegisterView, ProfileView, 
-                    ChangePasswordView, ChangeEmailView)
+from .views import *
 from rest_framework_simplejwt import views as jwt_views # type: ignore
 
 urlpatterns = [
@@ -10,4 +9,18 @@ urlpatterns = [
     path('profile/', ProfileView.as_view(), name='profile'),
     path('change-password/', ChangePasswordView.as_view(), name='change_password'),
     path('change-email/', ChangeEmailView.as_view(), name='change_email'),
+    path('login/', login, name='login'),
+    path('index/', index, name='index'),
+    path('product/<int:pk>/', product, name="product"),
+
+    path('resetpass/',resetpass, name='resetpass'),
+    path('dashboard/', dashboard, name='dashboard'),
+    path('dashboard/profile/', edit_profile, name='edit_profile'),
+    path('dashboard/password/', change_password, name='change_password'),
+    path('message/', message, name="message"),
+    path('support/', support, name="support"),
+    path('logout/', logout, name="logout")
+
+
+
 ]
