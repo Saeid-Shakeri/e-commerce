f4a4a84d0a42e18b4b9fafe36dbc53e18f3ed522
Saeid-Shakeri <saeidshakeribcm1@gmail.com>
2024-11-23 14:08:10 +0330

    added a slash

--- product/commodity/urls.py
+++ product/commodity/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('products/', ProductListView.as_view(), name='product-list'),
     path('product_search/', ProductSearchView.as_view(), name='product-search'),
     path('products/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
-    path('category-admin', CategoryListCreateView.as_view(), name='category-admin'),
+    path('category-admin/', CategoryListCreateView.as_view(), name='category-admin'),
     path('product-list-admin/', ProductListCreateView.as_view(), name='product-list-admin'),
     path('product-detail-admin/', ProductDetailAdminView.as_view(), name='product-detail-admin'),
 
